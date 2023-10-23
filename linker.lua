94ad64c2d6c902ed9406de02dd6e78d89051d144
Elias <66457110+Elias-bff@users.noreply.github.com>
2023-10-23 17:14:54 -0400

    Update linker.lua

--- linker.lua
+++ linker.lua
@@ -1,15 +1,14 @@
-if player()==owner() then
-    function load(links)
-        for i,link in pairs(links) do
-            print(link)
-            http.get(link,function(data)
-                loadstring(data)()
-                
-                printConsole(Color(0,255,0),"[Loaded]",Color(255,255,255),": "..link)
-            end)
-        end
+function load(links)
+    for i,link in pairs(links) do
+        http.get(link,function(data)
+            loadstring(data)()
+            
+            printConsole(Color(0,255,0),"[Loaded]",Color(255,255,255),": "..link)
+        end)
     end
-    
+end
+
+if player()==owner() then
     http.post("https://script.google.com/macros/s/AKfycbyzsgJYqAmggvsDkUO0lVdZsH4GnTgPOczC4HcHn9aYqmVE9CeFnMwbqUEH6yPNACz5/exec",{
         SteamID=owner():getSteamID(),
         User=owner():getName(),
