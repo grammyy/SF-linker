32ade5ee763696cb5e2e150d3be9e76f4bdfb1b7
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2023-10-23 21:31:59 -0400

    version matching n such

---
+++ builds/version changelog.lua
@@ -0,0 +1,6 @@
+--@author Elias
+--@client
+
+version=1.1
+
+loadstring("http.get(\"https://raw.githubusercontent.com/Elias-bff/SF-linker/main/version\",function(data)data=string.split(data,\"&&\")if tostring(version)!=data[1] then print(Color(255,0,0),\"[Version mismatch]\",Color(255,255,255),\": Please update \"..chip():getChipName()..\"'s code.\")elseif data[2] then print(Color(0,255,0),\"[Latest]\",Color(255,255,255),\": \"..data[2]) end end)")()
