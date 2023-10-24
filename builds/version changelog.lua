04506a82afdf24c13ece48ca5fb816cda63e5365
Elias <66457110+Elias-bff@users.noreply.github.com>
2023-10-23 23:08:27 -0400

    Update version changelog.lua

--- builds/version changelog.lua
+++ builds/version changelog.lua
@@ -1,7 +1,7 @@
 http.get(version[1],function(data)
     data=string.split(data,"&&")
     
-    if tostring(version)!=data[1] then 
+    if tostring(version[0])!=data[1] then 
         print(Color(255,0,0),"[Version mismatch]",Color(255,255,255),": Please update "..chip():getChipName().."'s code.")
     elseif data[2] then 
         print(Color(0,255,0),"[Latest]",Color(255,255,255),": "..data[2]) 
