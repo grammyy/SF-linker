fad63ef7a7c5e33520fada3dcea0c32bfddfb61e
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2023-10-23 21:53:13 -0400

    Update version changelog.lua

--- builds/version changelog.lua
+++ builds/version changelog.lua
@@ -1,6 +1,9 @@
---@author Elias
---@client
-
-version=1.1
-
-loadstring("http.get(\"https://raw.githubusercontent.com/Elias-bff/SF-linker/main/builds/version\",function(data)data=string.split(data,\"&&\")if tostring(version)!=data[1] then print(Color(255,0,0),\"[Version mismatch]\",Color(255,255,255),\": Please update \"..chip():getChipName()..\"'s code.\")elseif data[2] then print(Color(0,255,0),\"[Latest]\",Color(255,255,255),\": \"..data[2]) end end)")()
+http.get("https://raw.githubusercontent.com/Elias-bff/SF-linker/main/builds/version",function(data)
+    data=string.split(data,"&&")
+    
+    if tostring(version)!=data[1] then 
+        print(Color(255,0,0),"[Version mismatch]",Color(255,255,255),": Please update "..chip():getChipName().."'s code.")
+    elseif data[2] then 
+        print(Color(0,255,0),"[Latest]",Color(255,255,255),": "..data[2]) 
+    end 
+end)
