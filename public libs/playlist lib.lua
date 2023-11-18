7643304a4d3dbfeebf3f14a04f1497af7136fd18
Elias-bff <bartender.winery@gmail.com>
2023-11-17 21:29:09 -0500

    Update playlist lib.lua

--- public libs/playlist lib.lua
+++ public libs/playlist lib.lua
@@ -30,7 +30,7 @@ if SERVER then
         
         net.start("cl_sync")
         net.writeTable(packet)
-        net.writeString(ply:getString())
+        net.writeString(ply:getName())
         net.send(packet[2] or nil)
     end)
     
