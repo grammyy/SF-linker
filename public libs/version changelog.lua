26d7a62de4da0f33d8b529f8bc1b4c53490032f0
Elias-bff <bartender.winery@gmail.com>
2023-11-22 00:03:28 -0500

    Update version changelog.lua

--- public libs/version changelog.lua
+++ public libs/version changelog.lua
@@ -2,8 +2,8 @@ http.get(repo,function(data)
     data=string.split(data,"&&")
     
     if tostring(version)!=data[1] then 
-        print(Color(255,0,0),"[Version mismatch]",Color(255,255,255),": Please update "..chip():getChipName().."'s code.")
-        print("("..data[1]..") Changelog: "..data[2])
+        print(Color(255,0,0),"[Version mismatch]",Color(255,255,255),": Please update "..chip():getChipName().."'s code.\n")
+        print(Color(255,0,0),"[Latest: "..data[1].."]",Color(255,255,255)," Changelog: "..data[2])
     elseif data[2] then 
         print(Color(0,255,0),"[Latest]",Color(255,255,255),": "..data[2]) 
     end 
