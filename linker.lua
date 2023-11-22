8481406c11b31ce16308d4a900d58a00f1272599
Elias-bff <bartender.winery@gmail.com>
2023-11-21 23:13:53 -0500

    queuing added

--- linker.lua
+++ linker.lua
@@ -1,4 +1,20 @@
-loaded={}
+local function queue(func)
+    if !_queue then
+        _queue={[1]=func}
+        
+        timer.create("queue_"..1/3,1/3,0,function()
+            if _queue[#_queue] then
+                _queue[#_queue]()
+                _queue[#_queue]=nil
+            else
+                timer.remove("queue_"..1/3)
+                _queue=nil
+            end
+        end)
+    else
+        table.insert(_queue,1,func)
+    end
+end
 
 function load(links)
     for link,callback in pairs(links) do
@@ -6,23 +22,23 @@ function load(links)
             link=callback
         end
 
-        http.get(link.."?time="..timer.realtime(),function(data)
-            local name=string.split(http.urlDecode(link),"/")
-            func,error=loadstring(data)
-
-            if (!func or error) and CLIENT then
-                printConsole(Color(255,0,0),"[Failed: "..(SERVER and "SERVER" or "CLIENT").."]",Color(255,255,255),": "..link.." ; ",Color(255,0,0),func,Color(255,255,255)," ; ",error)
-            else
-                func()
-
-                loaded[name[#name]]=true
-            
-                if isfunction(callback) then
-                    callback()
+        queue(1/3,function()
+            http.get(link.."?time="..timer.realtime(),function(data)
+                local name=string.split(http.urlDecode(link),"/")
+                func,error=loadstring(data)
+    
+                if (!func or error) and CLIENT then
+                    printConsole(Color(255,0,0),"[Failed: "..(SERVER and "SERVER" or "CLIENT").."]",Color(255,255,255),": "..link.." ; ",Color(255,0,0),func,Color(255,255,255)," ; ",error)
+                else
+                    func()
+    
+                    if isfunction(callback) then
+                        callback()
+                    end
+                        
+                    printConsole(Color(0,255,0),"[Loaded: "..(SERVER and "SERVER" or "CLIENT").."]",Color(255,255,255),": "..link)
                 end
-                    
-                printConsole(Color(0,255,0),"[Loaded: "..(SERVER and "SERVER" or "CLIENT").."]",Color(255,255,255),": "..link)
-            end
+            end)
         end)
     end
 end
